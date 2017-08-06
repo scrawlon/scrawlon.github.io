84f7f5cb1ba3256867fc2f26afe95d2a49e5a345
scrawlon <acid64k@yahoo.com>
2017-08-06 13:35:32 -0400

    Working on mobile nav menu

---
+++ _js/app.js
@@ -0,0 +1,33 @@
+var mobileNav = document.getElementById('nav-mobile');
+var mobileNavToggle = document.getElementById('nav-mobile-toggle');
+var mobileNavBackdrop = document.getElementById('nav-mobile-backdrop');
+var html = document.querySelector('html');
+var body = document.querySelector('body');
+
+console.log('mobile nav', mobileNav);
+console.log('mobile nav toggle', mobileNavToggle);
+
+mobileNavToggle.onclick = function(e) {
+  console.log('click');
+
+  mobileNavBackdrop.classList.add('visible');
+
+  [html, body].forEach(function(el) {
+    el.classList.add('no-scroll');
+  });
+
+  mobileNav.animate(mobileNavSlideInAnimation, {
+    duration: 200,
+    easing: 'ease-in-out',
+    fill: 'forwards'
+  });
+}
+
+var mobileNavSlideInAnimation = [
+  {
+    transform: 'translate(100vw, 0)'
+  },
+  {
+    transform: 'translate(0vw, 0)'
+  }
+]
---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
+
+gulp.task('browserify', function() {
+  var b = browserify({
+    entries: './_js/app.js',
+    debug: true,
+  });
+
+  return b.bundle()
+    .pipe(source('app.js'))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./_js/*.js', ['browserify']);
+});
