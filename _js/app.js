e145b212c112befee88286ca6a44468b4d4bb2cf
scrawlon <acid64k@yahoo.com>
2017-08-07 23:01:23 -0400

    mobile menu now opens and closes

--- _js/app.js
+++ _js/app.js
@@ -3,6 +3,8 @@ var mobileNavToggle = document.getElementById('nav-mobile-toggle');
 var mobileNavBackdrop = document.getElementById('nav-mobile-backdrop');
 var html = document.querySelector('html');
 var body = document.querySelector('body');
+var mobileNavOpen = false;
+var direction = 'normal';
 
 console.log('mobile nav', mobileNav);
 console.log('mobile nav toggle', mobileNavToggle);
@@ -10,19 +12,33 @@ console.log('mobile nav toggle', mobileNavToggle);
 mobileNavToggle.onclick = function(e) {
   console.log('click');
 
+  if ( mobileNavOpen === 'true' ) {
+    direction = 'reverse';
+    mobileNavOpen = 'false';
+
+    [html, body].forEach(function(el) {
+      el.classList.remove('no-scroll');
+    });
+  } else {
+    direction = 'normal';
+    mobileNavOpen = 'true';
+
+    [html, body].forEach(function(el) {
+      el.classList.add('no-scroll');
+    });
+  }
+
   mobileNavBackdrop.classList.add('visible');
 
   mobileNavBackdrop.animate(mobileNavBackdropAnimation, {
+    direction: direction,
     duration: 300,
     easing: 'ease-in-out',
     fill: 'forwards'
   });
 
-  [html, body].forEach(function(el) {
-    el.classList.add('no-scroll');
-  });
-
   mobileNav.animate(mobileNavSlideInAnimation, {
+    direction: direction,
     duration: 400,
     easing: 'ease-in-out',
     fill: 'forwards'
