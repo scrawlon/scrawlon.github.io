321d632eacbf47f53948a4dd2c82c8b7e392c3f4
scrawlon <acid64k@yahoo.com>
2017-08-19 15:10:18 -0400

    Mobile nav closes on browser resize

--- _js/app.js
+++ _js/app.js
@@ -81,3 +81,51 @@ function animatedMenuOut(direction) {
     mobileNavBackdrop.classList.remove('visible');
   }
 }
+
+/* requestAnimationFrame + customEvent
+ * from https://developer.mozilla.org/en-US/docs/Web/Events/resize
+ */
+
+(function() {
+    var throttle = function(type, name, obj) {
+        obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+             requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+
+    /* init - you can init any event */
+    throttle("resize", "optimizedResize");
+})();
+
+// handle event
+window.addEventListener("optimizedResize", function () {
+  mobileNavOpen && mobileMenuAnimate();
+});
+
+/* ie 11 Custom EVent polyfill
+ * from https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+ */
+
+ (function () {
+
+   if ( typeof window.CustomEvent === "function" ) return false;
+
+   function CustomEvent ( event, params ) {
+     params = params || { bubbles: false, cancelable: false, detail: undefined };
+     var evt = document.createEvent( 'CustomEvent' );
+     evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+     return evt;
+    }
+
+   CustomEvent.prototype = window.Event.prototype;
+
+   window.CustomEvent = CustomEvent;
+ })();
