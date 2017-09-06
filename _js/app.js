8bbd3f55049009f21da72826328e9c598c81380b
scrawlon <acid64k@yahoo.com>
2017-09-06 18:24:27 -0400

    GET portfolio-project.json working on dev

--- _js/app.js
+++ _js/app.js
@@ -1,2 +1,3 @@
 var mobileMenu = require('./mobileMenu');
 var homeSlider = require('./homeSlider');
+var reactPortfolioDev = require('./react-portfolio/build/static/js/bundle.js');
--- _js/react-portfolio/src/App.js
+++ _js/react-portfolio/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Portfolio />
         <div className="App-header">
           <a href="https://facebook.github.io/react/" title="React" target="_blank" rel="noopener noreferrer">
             <h2>Built with React</h2>
@@ -13,6 +15,40 @@ class App extends Component {
           </a>
         </div>
       </div>
+    );
+  }
+}
+
+class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      projects: []
+    };
+  }
+
+  componentDidMount () {
+    this.setState((prevState) => {
+      return { loading: true };
+    });
+
+    axios.get('http://localhost:4000/portfolio-projects.json')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const loading = this.state.loading;
+
+    return (
+      !loading
+        ? <h2>Portfolio</h2>
+        : <h2>Loading...</h2>
     );
   }
 }
