b7fa8a21cd458c2d7f7eb24c14a9863276b61e30
scrawlon <acid64k@yahoo.com>
2017-09-06 22:55:25 -0400

    Tag clouds

--- _js/react-portfolio/src/components/PortfolioFilterBar.js
+++ _js/react-portfolio/src/components/PortfolioFilterBar.js
@@ -11,9 +11,9 @@ class PortfolioFilterBar extends Component {
     projects.forEach((project) => {
       project.tags.forEach((tag) => {
         if ( tag.industries ) {
-          tags.industries = [...new Set(tags.industries.concat(tag.industries))];
+          tags.industries = [...new Set(tags.industries.concat(tag.industries))].sort();
         } else if ( tag.technologies ) {
-          tags.technologies = [...new Set(tags.technologies.concat(tag.technologies))];
+          tags.technologies = [...new Set(tags.technologies.concat(tag.technologies))].sort((a,b) => sortAlpha(a,b));
         }
       });
     });
@@ -22,14 +22,14 @@ class PortfolioFilterBar extends Component {
 
     return (
       <div>
-        <ul>
-          <h2>Industries</h2>
+        <ul className="tag-cloud">
+          <h3>Tags: Industries</h3>
           {tags.industries.length && tags.industries.map((tag) => {
             return <li key={tag}>{tag}</li>;
           })}
         </ul>
-        <ul>
-          <h2>Technologies</h2>
+        <ul className="tag-cloud">
+          <h3>Tags: Technologies</h3>
           {tags.technologies.length && tags.technologies.map((tag) => {
             return <li key={tag}>{tag}</li>;
           })}
@@ -37,6 +37,14 @@ class PortfolioFilterBar extends Component {
       </div>
     );
   }
+}
+
+function sortAlpha(a,b) {
+  let aLower = a && a.toLowerCase();
+  let bLower = b && b.toLowerCase();
+  if (aLower < bLower) return -1;
+  else if (aLower > bLower) return 1;
+  return 0;
 }
 
 export default PortfolioFilterBar;
