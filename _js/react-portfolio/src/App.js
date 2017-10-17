7aba60ee818af98af183e988d01dc3a6218f4217
scrawlon <acid64k@yahoo.com>
2017-10-16 20:46:18 -0400

    Porfolio. Scroll position fixed

--- _js/react-portfolio/src/App.js
+++ _js/react-portfolio/src/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
-import ScrollToTop from './helpers/ScrollToTop';
+import ScrollMemory from 'react-router-scroll-memory';
 import Portfolio from './components/Portfolio';
 import logo from './logo.svg';
 import './App.css';
@@ -11,11 +11,12 @@ import './App.css';
 const App = () => (
   <div className="App">
     <Router>
-      <ScrollToTop>
-        <Route render={({ location }) => (
+      <Route render={({ location }) => (
+        <div>
+          <ScrollMemory />
           <Portfolio />
-        )} />
-      </ScrollToTop>
+        </div>
+      )} />
     </Router>
     <div className="App-footer">
       <a href="https://facebook.github.io/react/" title="React" target="_blank" rel="noopener noreferrer">
