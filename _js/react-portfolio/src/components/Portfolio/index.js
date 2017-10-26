0202e0f393a97672030a81dc5d2d175eb9b6f402
scrawlon <acid64k@yahoo.com>
2017-10-25 20:53:12 -0400

    Portfolio. Reinstalled react-router-dom. Code cleanup

--- _js/react-portfolio/src/components/Portfolio/index.js
+++ _js/react-portfolio/src/components/Portfolio/index.js
@@ -56,15 +56,4 @@ class Portfolio extends Component {
   }
 }
 
-function debounce(fn, delay) {
-  var timer = null;
-  return function () {
-    var context = this, args = arguments;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      fn.apply(context, args);
-    }, delay);
-  };
-}
-
 export default Portfolio;
