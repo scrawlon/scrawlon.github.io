474902d366c292749f6bff3cd7a494182cec2d55
scrawlon <acid64k@yahoo.com>
2017-11-07 21:18:28 -0500

    Portfolio. matching css between react and jekyll

--- _js/app.js
+++ _js/app.js
@@ -1,4 +1,4 @@
 var mobileMenu = require('./mobileMenu');
 var homeSlider = require('./homeSlider');
-/*var reactPortfolioDev = require('./portfolio.js');*/
-var reactPortfolioDev = require('./react-portfolio/build/static/js/main.js');
+var reactPortfolioDev = require('./portfolio.js');
+/*var reactPortfolioDev = require('./react-portfolio/build/static/js/main.js');*/
--- _js/react-portfolio/src/App.js
+++ _js/react-portfolio/src/App.js
@@ -15,12 +15,6 @@ const App = () => (
         <Portfolio />
       </div>
     </Router>
-    <div className="App-footer">
-      <a href="https://facebook.github.io/react/" title="React" target="_blank" rel="noopener noreferrer">
-        <h3>Built with React</h3>
-        <img src={logo} className="App-logo" alt="logo" />
-      </a>
-    </div>
   </div>
 );
 
