7d3c58828898f46c7b7ef1c0e3a001d474c5e91e
scrawlon <acid64k@yahoo.com>
2017-11-12 23:17:46 -0500

    Portfolio. Load React app only on Portfolio page

--- _js/react-portfolio/src/index.js
+++ _js/react-portfolio/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+const appContainer = document.getElementById('root');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+if ( appContainer ) {
+  ReactDOM.render(<App />, appContainer);
+  registerServiceWorker();
+}
