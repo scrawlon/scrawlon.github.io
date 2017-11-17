6ced1da2b6cdc0a939e6186e876ed4b6e8c0b64f
scrawlon <acid64k@yahoo.com>
2017-11-16 22:22:05 -0500

    Portfolio. Details CSS

--- _js/react-portfolio/src/components/Portfolio/PortfolioDetails.js
+++ _js/react-portfolio/src/components/Portfolio/PortfolioDetails.js
@@ -7,50 +7,70 @@ class PortfolioDetails extends Component {
     const project = this.props.projects.find((project) => {
       return project.id === this.props.match.params.id;
     });
+    const headerBackground = {
+      background: '#000',
+      color: '#FFF',
+      height: 'auto',
+      padding: '5px 0',
+      margin: '0 0 10px',
+      textAlign: 'center'
+    };
 
     if ( !project ) { return <NotFound /> }
 
     return (
-      <div className="portfolio-wrapper">
-        <div className="portfolio-details-wrapper">
-          {project
-            ?
-            <article>
+      <div>
 
-              <section className="headline">
-                <h1>"{project.title}"</h1>
-                <img className="header-image" src={project.screenshot} alt={project.title} />
-              </section>
+        <header style={headerBackground}>
+          <div className="wrapper">
+            <h1 className="page">
+              Portfolio: "{project.title}"
+            </h1>
+          </div>
+        </header>
 
-              <section className="content">
-                <h2>Quick Links</h2>
-                <ul>
-                  <li>Project Hompage: <a href={project.site_url} alt={project.title} target="_blank">{project.title}</a></li>
-                  {project.my_work
-                    ? <li>My Main Contribution: <a href={project.my_work.page_url} alt={project.title + ": " + project.my_work.page_title} target="_blank">{project.title + ": " + project.my_work.page_title}</a></li>
-                    : ''}
-                </ul>
+        <div className="portfolio-wrapper">
+          <div className="portfolio-details-wrapper">
+            {project
+              ?
+              <article>
 
-                <h2>Project Notes</h2>
-                <div className="portfolio-details-text" dangerouslySetInnerHTML={ {__html: project.content} } />
-              </section>
+                <section className="headline">
+                  <p className="portfolio-details-agency">Design Agency: {project.employer}</p>
+                  <img className="header-image" src={project.screenshot} alt={project.title} />
+                </section>
 
-              <aside>
-                <h3>Project Types</h3>
-                <PortfolioTags tags={project.tags} tagTypes={['project_types']}/>
+                <section className="content">
 
-                <h3>Technologies</h3>
-                <PortfolioTags tags={project.tags} tagTypes={['technologies']}/>
+                  <h2>Quick Links</h2>
+                  <ul>
+                    <li>Project Hompage: <a href={project.site_url} alt={project.title} target="_blank">{project.title}</a></li>
+                    {project.my_work
+                      ? <li>My Main Contribution: <a href={project.my_work.page_url} alt={project.title + ": " + project.my_work.page_title} target="_blank">{project.title + ": " + project.my_work.page_title}</a></li>
+                      : ''}
+                  </ul>
 
-                <h3>Industries</h3>
-                <PortfolioTags tags={project.tags} tagTypes={['industries']}/>
-              </aside>
+                  <h2>Project Notes</h2>
+                  <div className="portfolio-details-text" dangerouslySetInnerHTML={ {__html: project.content} } />
+                </section>
 
-            </article>
-            : ''
-          }
+                <aside>
+                  <h3>Project Types</h3>
+                  <PortfolioTags tags={project.tags} tagTypes={['project_types']}/>
+
+                  <h3>Technologies</h3>
+                  <PortfolioTags tags={project.tags} tagTypes={['technologies']}/>
+
+                  <h3>Industries</h3>
+                  <PortfolioTags tags={project.tags} tagTypes={['industries']}/>
+                </aside>
+
+              </article>
+              : ''
+            }
+          </div>
         </div>
-      </div>  
+      </div>
     );
   }
 }
