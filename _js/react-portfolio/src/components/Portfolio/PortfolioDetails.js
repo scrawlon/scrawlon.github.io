37c9eb97d105feb509ef412d4c2dfd5e6e531e75
scrawlon <acid64k@yahoo.com>
2017-11-16 21:02:50 -0500

    Portfolio. Working on header

--- _js/react-portfolio/src/components/Portfolio/PortfolioDetails.js
+++ _js/react-portfolio/src/components/Portfolio/PortfolioDetails.js
@@ -11,44 +11,46 @@ class PortfolioDetails extends Component {
     if ( !project ) { return <NotFound /> }
 
     return (
-      <div className="portfolio-details-wrapper">
-        {project
-          ?
-          <article>
+      <div className="portfolio-wrapper">
+        <div className="portfolio-details-wrapper">
+          {project
+            ?
+            <article>
 
-            <section className="headline">
-              <h1>"{project.title}"</h1>
-              <img className="header-image" src={project.screenshot} alt={project.title} />
-            </section>
+              <section className="headline">
+                <h1>"{project.title}"</h1>
+                <img className="header-image" src={project.screenshot} alt={project.title} />
+              </section>
 
-            <section className="content">
-              <h2>Quick Links</h2>
-              <ul>
-                <li>Project Hompage: <a href={project.site_url} alt={project.title} target="_blank">{project.title}</a></li>
-                {project.my_work
-                  ? <li>My Main Contribution: <a href={project.my_work.page_url} alt={project.title + ": " + project.my_work.page_title} target="_blank">{project.title + ": " + project.my_work.page_title}</a></li>
-                  : ''}
-              </ul>
+              <section className="content">
+                <h2>Quick Links</h2>
+                <ul>
+                  <li>Project Hompage: <a href={project.site_url} alt={project.title} target="_blank">{project.title}</a></li>
+                  {project.my_work
+                    ? <li>My Main Contribution: <a href={project.my_work.page_url} alt={project.title + ": " + project.my_work.page_title} target="_blank">{project.title + ": " + project.my_work.page_title}</a></li>
+                    : ''}
+                </ul>
 
-              <h2>Project Notes</h2>
-              <div className="portfolio-details-text" dangerouslySetInnerHTML={ {__html: project.content} } />
-            </section>
+                <h2>Project Notes</h2>
+                <div className="portfolio-details-text" dangerouslySetInnerHTML={ {__html: project.content} } />
+              </section>
 
-            <aside>
-              <h3>Project Types</h3>
-              <PortfolioTags tags={project.tags} tagTypes={['project_types']}/>
+              <aside>
+                <h3>Project Types</h3>
+                <PortfolioTags tags={project.tags} tagTypes={['project_types']}/>
 
-              <h3>Technologies</h3>
-              <PortfolioTags tags={project.tags} tagTypes={['technologies']}/>
+                <h3>Technologies</h3>
+                <PortfolioTags tags={project.tags} tagTypes={['technologies']}/>
 
-              <h3>Industries</h3>
-              <PortfolioTags tags={project.tags} tagTypes={['industries']}/>
-            </aside>
+                <h3>Industries</h3>
+                <PortfolioTags tags={project.tags} tagTypes={['industries']}/>
+              </aside>
 
-          </article>
-          : ''
-        }
-      </div>
+            </article>
+            : ''
+          }
+        </div>
+      </div>  
     );
   }
 }
--- _js/react-portfolio/src/components/Portfolio/PortfolioList.js
+++ _js/react-portfolio/src/components/Portfolio/PortfolioList.js
@@ -8,26 +8,42 @@ import PortfolioTags from './PortfolioTags';
 class PortfolioList extends Component {
   render() {
     const projects = this.props.projects;
+    const headerBackground = {
+      backgroundImage: 'url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAG0lEQVQYV2NMKL/ty4ADMIIkF3SqbsYmP+gkAayXGgfe8HOVAAAAAElFTkSuQmCC)'
+    };
 
     return (
       <div>
-        <PortfolioFilterBar projects={projects} />
-        <ul className="portfolio-list">
-          {projects.length && projects.map((project) => {
-            const imageClass = "project-image" + (project.screenshot_small ? " small" : "");
-            return (
-              <Link to={`${this.props.match.url}${project.id}`} key={project.id}>
-                <li key={project.title}>
-                  <div className={imageClass} style={{backgroundImage: 'url(' + project.screenshot + ')'}}></div>
-                  <div className="project-details">
-                    <h3>"{project.title}"</h3>
-                    <PortfolioTags tags={project.tags} tagTypes={['technologies']}/>
-                  </div>
-                </li>
-              </Link>
-            );
-          })}
-        </ul>
+
+        <header style={headerBackground}>
+          <div className="backdrop">
+          </div>
+          <div className="wrapper">
+            <h1 className="page">
+              Portfolio
+            </h1>
+          </div>
+        </header>
+
+        <div className="portfolio-wrapper">
+          <PortfolioFilterBar projects={projects} />
+          <ul className="portfolio-list">
+            {projects.length && projects.map((project) => {
+              const imageClass = "project-image" + (project.screenshot_small ? " small" : "");
+              return (
+                <Link to={`${this.props.match.url}${project.id}`} key={project.id}>
+                  <li key={project.title}>
+                    <div className={imageClass} style={{backgroundImage: 'url(' + project.screenshot + ')'}}></div>
+                    <div className="project-details">
+                      <h3>"{project.title}"</h3>
+                      <PortfolioTags tags={project.tags} tagTypes={['technologies']}/>
+                    </div>
+                  </li>
+                </Link>
+              );
+            })}
+          </ul>
+        </div>  
       </div>
     );
   }
