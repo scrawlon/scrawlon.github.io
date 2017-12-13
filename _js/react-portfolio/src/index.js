82fd4469a4dcc0bb74f52fe10aaeae9464b5a3c4
scrawlon <acid64k@yahoo.com>
2017-12-13 07:21:05 -0500

    React. Remove service-worker.js

--- _js/react-portfolio/src/index.js
+++ _js/react-portfolio/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+/*import registerServiceWorker from './registerServiceWorker';*/
 const appContainer = document.getElementById('root');
 
 if ( appContainer ) {
   ReactDOM.render(<App />, appContainer);
-  registerServiceWorker();
+  /*registerServiceWorker();*/
 }
