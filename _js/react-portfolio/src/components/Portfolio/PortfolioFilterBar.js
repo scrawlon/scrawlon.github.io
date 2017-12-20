73d11a3ee9fd0ff419b4aa8315a4214f95909f49
scrawlon <acid64k@yahoo.com>
2017-12-19 22:22:32 -0500

    Portfolio. React filter

--- _js/react-portfolio/src/components/Portfolio/PortfolioFilterBar.js
+++ _js/react-portfolio/src/components/Portfolio/PortfolioFilterBar.js
@@ -10,15 +10,65 @@ class PortfolioFilterBar extends Component {
         technologies: false
       }
     }
+
+    this.handleFilterSelect = this.handleFilterSelect.bind(this);
   }
+
+  handleFilterSelect(event) {
+    const projectFilters = Object.keys(this.state.filtersVisible);
+    const filtersVisible = this.state.filtersVisible;
+    const filterChange = event.target.value;
+
+    projectFilters.forEach((tagType) => {
+      if ( tagType === filterChange ) {
+        filtersVisible[tagType] = !this.state.filtersVisible[tagType];
+      } else {
+        filtersVisible[tagType] = false;
+      }
+    });
+
+
+    this.setState({
+      filterVisible: filtersVisible
+    });
+
+    console.log('handle filter select', this.state.filtersVisible);
+  }
+
   render() {
     const projectTags = this.props.projectTags;
+    const projectFilters = Object.keys(this.state.filtersVisible);
+
+    console.log('project filters', projectFilters);
 
     return (
       <div>
         <ul className="tag-cloud">
           <form action="">
             <h2>Filter Projects By:</h2>
+
+            <ul>
+              {!projectTags
+                ? ''
+                :
+                projectFilters.map((tagType, i) => {
+                  const tagLabel = tagType.replace('_', ' ').toUpperCase();
+                  const checked = this.state.filtersVisible[tagType];
+
+                  return (
+                    <li key={tagType}>
+                      <input
+                        type="radio"
+                        name={"filter-type" + (i + 1)}
+                        value={tagType}
+                        checked={checked ? 'checked' : ''}
+                        onChange={this.handleFilterSelect}/>
+                      <label htmlFor="filter-type">{tagLabel}</label>
+                    </li>
+                  );
+                })
+              }
+            </ul>
 
             <label htmlFor="tags-industries">Types</label>
             <select name="tags-project-types" multiple>
