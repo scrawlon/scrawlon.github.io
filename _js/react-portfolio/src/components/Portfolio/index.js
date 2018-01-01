51e891df4cce757ba67b87e666a6cec46dd63cb5
scrawlon <acid64k@yahoo.com>
2018-01-01 01:23:14 -0500

    Portfolio. Removed main loading message

--- _js/react-portfolio/src/components/Portfolio/index.js
+++ _js/react-portfolio/src/components/Portfolio/index.js
@@ -50,14 +50,11 @@ class Portfolio extends Component {
     const projectTags = this.state.projectTags;
 
     return (
-      !loading
-        ?
-        <Switch>
-          <Route exact path = "/" render={(props) => <PortfolioList {...props} projects={projects} projectTags={projectTags} />} />
-          <Route exact path="/:id" render={(props) => <PortfolioDetails {...props} projects={projects} />} />
-          <Route path="*" status="404" component={NotFound} />
-        </Switch>
-        : <h2>Loading...</h2>
+      <Switch>
+        <Route exact path = "/" render={(props) => <PortfolioList {...props} projects={projects} projectTags={projectTags} />} />
+        <Route exact path="/:id" render={(props) => <PortfolioDetails {...props} projects={projects} />} />
+        <Route path="*" status="404" component={NotFound} />
+      </Switch>
     );
   }
 }
