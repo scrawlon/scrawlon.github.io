b85083e6a670ab9e12fd37871431267202603f9c
scrawlon <acid64k@yahoo.com>
2018-01-13 01:25:39 -0500

    Homepage slider animation progress

--- _js/velocity-animation.js
+++ _js/velocity-animation.js
@@ -3,6 +3,10 @@ var Velocity = require('velocity-animate');
 console.log('velocity-animation', 2);
 
 (function() {
+  var svgScrawlonLogoFull = document.getElementsByClassName('scrawlon-logo-full');
+  var svgScrawlonLogoFullGradientOne = document.getElementById('slf-bg-one');
+  var svgScrawlonLogoFullGradientTwo = document.getElementById('slf-bg-two');
+  var svgScrawlonLogoFullGradientThree = document.getElementById('slf-bg-three');
   var svgLogoFrame = document.getElementsByClassName('logo-frame');
   var swiperFade = document.getElementsByClassName('swiper-fade');
   var sliderBackground = document.getElementsByClassName('slider-background');
@@ -10,6 +14,10 @@ console.log('velocity-animation', 2);
   /*Velocity(swiperFade, { background: "linearGradient(to bottom, transparent, rgba(25,25,112,1))" });*/
   /*Velocity(swiperFade, { background: "linearGradient(to bottom, transparent, rgba(25,25,112,0.6))" }, { duration: 2000, delay: 1000 });*/
   Velocity(sliderBackground, { opacity: 1 }, { duration: 1000, delay: 500 });
-  Velocity(swiperFade, { opacity: .5 }, { duration: 5000, delay: 500 });
+  /*Velocity(swiperFade, { opacity: .5 }, { duration: 5000, delay: 500 });*/
   Velocity(svgLogoFrame, { 'stroke-dasharray': 494 }, { duration: 1000, delay: 900 }, 'easeInOutSine');
+  Velocity(svgScrawlonLogoFullGradientOne, { stopOpacity: .6 }, { duration: 1500, delay: 1850 });
+  Velocity(svgScrawlonLogoFullGradientTwo, { stopOpacity: .6 }, { duration: 1500, delay: 1850 });
+  Velocity(svgScrawlonLogoFullGradientThree, { stopOpacity: .8 }, { duration: 1500, delay: 1850 });
+  /*Velocity(svgScrawlonLogoFull, { opacity: 0 }, { duration: 1000, delay: 1500 });*/
 })();
