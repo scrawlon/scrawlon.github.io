124f8b2c1e9c209ed0c8503220f495d97b919687
scrawlon <acid64k@yahoo.com>
2018-01-14 23:09:33 -0500

    Home slider. Word animation working

--- _js/velocity-animation.js
+++ _js/velocity-animation.js
@@ -7,6 +7,7 @@ var Velocity = require('velocity-animate');
   var svgSo = document.getElementsByClassName('logo-so');
   var svgScrawlon = document.getElementsByClassName('logo-scrawlon');
   var svgLogoFrame = document.getElementsByClassName('logo-frame');
+  var swiperColorFrame = document.getElementsByClassName('swiper-color-frame');
   var swiperFade = document.getElementsByClassName('swiper-fade');
   var sliderBackground = document.getElementsByClassName('slider-background');
 
@@ -18,23 +19,37 @@ var Velocity = require('velocity-animate');
       endAt: 2500
     },*/
 
+    /* word art */
+    {
+      animation: wordArt,
+      startAt: 500,
+      endAt: 8500
+    },
+
+    /* Swiper Color Frame */
+    {
+      animation: fadeOutSwiperColorFrame,
+      startAt: 2500,
+      endAt: 6500
+    },
+
     /* svgLogoFrame */
     {
       animation: fadeInsScrawlonLogoFull,
-      startAt: 500,
-      endAt: 2500
+      startAt: 8500,
+      endAt: 10500
     },
     {
       animation: drawLogoFrame,
-      startAt: 900,
-      endAt: 2900
+      startAt: 8900,
+      endAt: 10900
     },
 
     /* svgSo */
     {
       animation: drawLogoSoOutline,
-      startAt: 2000,
-      endAt: 2800
+      startAt: 10000,
+      endAt: 10800
     },
     {
       animation: drawLogoSoFill,
@@ -45,15 +60,15 @@ var Velocity = require('velocity-animate');
     /* logo gradients */
     {
       animation: fadeInLogoGradient,
-      startAt: 3900,
-      endAt: 5000
+      startAt: 11900,
+      endAt: 13000
     },
 
     /* svgScrawlon */
     {
       animation: drawLogoScrawlon,
-      startAt: 5200,
-      endAt: 6200
+      startAt: 13200,
+      endAt: 14200
     }
   ];
 
@@ -64,6 +79,34 @@ var Velocity = require('velocity-animate');
   /*function fadeInBackground(startAt, endAt) {
     Velocity(sliderBackground, { opacity: 1 }, { duration: endAt - startAt, delay: startAt });
   }*/
+
+  function wordArt(startAt, endAt) {
+    var sentences = document.querySelectorAll('.slide-header p');
+    var totalDuration = endAt - startAt;
+    var individualDuration = totalDuration / sentences.length;
+    var fadeDuration = 500;
+
+    sentences.forEach(function(sentence, i) {
+      var spans = sentence.querySelectorAll('span');
+      var thisStart = startAt + (individualDuration * i);
+      /*var thisDuration = (individualDuration * (i + 1)) - fadeDuration;*/
+
+      Velocity(sentence, { opacity: 1 }, { duration: fadeDuration, delay: thisStart });
+
+      if ( spans.length ) {
+        var spanDuration = individualDuration / spans.length;
+        spans.forEach(function(span, i) {
+          Velocity(span, { opacity: 1 }, { duration: fadeDuration, delay: thisStart + (fadeDuration * 2) });
+        });
+      }
+
+      Velocity(sentence, 'reverse', { duration: fadeDuration, delay: individualDuration - (fadeDuration* 2) });
+    })
+  }
+
+  function fadeOutSwiperColorFrame(startAt, endAt) {
+    Velocity(swiperColorFrame, { opacity: .55 }, { duration: endAt - startAt, delay: startAt });
+  }
 
   function fadeInsScrawlonLogoFull(startAt, endAt) {
     Velocity(svgScrawlonLogoFull, { opacity: 1 }, { duration: endAt - startAt, delay: startAt });
