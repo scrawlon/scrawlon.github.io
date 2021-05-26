3af0824cdf13d3b28e1252b989617191fce5d6eb
Scott McGrath <scrawlon@gmail.com>
2021-05-25 20:09:42 -0400

    header CSS animation update

--- _js/app.js
+++ _js/app.js
@@ -2,13 +2,13 @@
 var urlPath = window.location.pathname;
 
 // require('./header');
-require('./mobileMenu');
-require('./homeSlider');
+// require('./mobileMenu');
+// require('./homeSlider');
 require('fg-loadcss');
 
-if ( urlPath.indexOf('/portfolio') !== -1 ) {
-  require('./portfolio');
-}
+// if ( urlPath.indexOf('/portfolio') !== -1 ) {
+//   require('./portfolio');
+// }
 
 // window.onload = function() {
 //   require('./velocity-animation');
