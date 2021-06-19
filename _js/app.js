e3b5d080a6af03b469e1d7ca81d295a4ec76956a
Scott McGrath <scrawlon@gmail.com>
2021-06-19 11:08:55 -0400

    Portfolio React app removed.

--- _js/app.js
+++ _js/app.js
@@ -1,15 +1,1 @@
-// var Velocity = require('velocity-animate');
-// var urlPath = window.location.pathname;
-
-// require('./header');
-// require('./mobileMenu');
-// require('./homeSlider');
-// require('fg-loadcss');
-
-// if ( urlPath.indexOf('/portfolio') !== -1 ) {
-//   require('./portfolio');
-// }
-
-// window.onload = function() {
-//   require('./velocity-animation');
-// }
+/* JS code goes here */
